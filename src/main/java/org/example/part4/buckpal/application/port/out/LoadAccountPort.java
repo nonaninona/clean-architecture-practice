7488efcef24abe4e46e1321346e9e2f7d38f9e7a
nonaninona <starcraft0529@gmail.com>
2024-11-21 19:18:24 +0900

    feat : part4. GetAccountBalanceQuery/Service 추가

--- src/main/java/org/example/part4/buckpal/application/port/out/LoadAccountPort.java
+++ src/main/java/org/example/part4/buckpal/application/port/out/LoadAccountPort.java
@@ -1,4 +1,9 @@
 package org.example.part4.buckpal.application.port.out;
 
+import java.time.LocalDateTime;
+import org.example.part4.buckpal.domain.Account;
+import org.example.part4.buckpal.domain.Account.AccountId;
+
 public interface LoadAccountPort {
+    Account loadAccount(AccountId accountId, LocalDateTime timestamp);
 }

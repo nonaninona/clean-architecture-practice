9a00f9ffc7c61eeea2e5319e704e828da9e01033
nonaninona <starcraft0529@gmail.com>
2024-11-30 16:36:56 +0900

    feat : part6. Account/ActivityRepository (JPA) 추가

---
+++ src/main/java/org/example/part6/buckpal/adapter/out/AccountRepository.java
@@ -0,0 +1,10 @@
+package org.example.part6.buckpal.adapter.out;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface AccountRepository extends JpaRepository<AccountJpaEntity, Long> {
+}
